05d40760de496c0078ebe025880ee75c522864bf
Nadia Chouiref <nadiachouiref@gmail.com>
2026-09-04 23:48:51 +0200

    Ajout smoke tests

---
+++ cypress/e2e/smoke.cy.ts
@@ -0,0 +1,14 @@
+describe('Smoke tests', () => {
+  it('affiche les champs et le bouton de connexion', () => {
+    cy.visit('/#/login');
+    cy.get('[data-cy=login-input-username]').should('exist');
+    cy.get('[data-cy=login-input-password]').should('exist');
+    cy.get('[data-cy=login-submit]').should('exist');
+  });
+
+  it('affiche le bouton d\'ajout au panier et la disponibilité du produit', () => {
+    cy.visit('/#/products/5');
+    cy.get('[data-cy=detail-product-add]').should('exist');
+    cy.get('[data-cy=detail-product-stock]').should('exist');
+  });
+});
