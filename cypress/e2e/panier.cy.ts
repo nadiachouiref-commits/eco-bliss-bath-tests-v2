824887614ff5d066d839a7e7b30bf19678c2580e
Nadia Chouiref <nadiachouiref@gmail.com>
2026-09-10 16:41:34 +0200

    Correction tests panier (ajout produit disponible, quantité supérieure au stock)

--- cypress/e2e/panier.cy.ts
+++ cypress/e2e/panier.cy.ts
@@ -49,27 +49,30 @@ describe('Panier', () => {
   });
 
   it('refuse une quantité supérieure à 20', () => {
-    cy.visit('/#/products/5');
+    cy.intercept('PUT', '**/orders/add').as('addToCart');
+    cy.visit('/#/products/7');
+    cy.get('[data-cy=detail-product-name]').should('not.be.empty');
     cy.get('[data-cy=detail-product-quantity]').clear().type('21');
     cy.get('[data-cy=detail-product-add]').click();
+    cy.wait('@addToCart');
     checkCartIsEmpty();
   });
 
   it('ajoute un produit disponible au panier', () => {
+    cy.intercept('GET', '**/products/5').as('getProduct');
     cy.visit('/#/products/5');
+    cy.wait('@getProduct');
 
-    // vérifie la présence du champ de disponibilité du produit
     cy.get('[data-cy=detail-product-stock]').should('exist');
 
-    cy.get('[data-cy=detail-product-stock]').invoke('text').then((stockText) => {
-      const initialStock = parseInt(stockText);
+    cy.get('[data-cy=detail-product-stock]').should('not.be.empty').invoke('text').then((stockText) => {
+      const initialStock = parseInt(stockText.replace(/\D/g, ''));
       cy.get('[data-cy=detail-product-name]').should('not.be.empty').invoke('text').then((productName) => {
         cy.get('[data-cy=detail-product-add]').click();
         cy.url().should('include', '/cart');
         cy.get('[data-cy=cart-line-name]').should('contain.text', productName);
         cy.get('[data-cy=cart-line-quantity]').should('have.value', '1');
 
-        // vérifie via l'API que le produit a bien été ajouté au panier
         cy.request({
           method: 'GET',
           url: 'http://localhost:8081/orders',
@@ -78,10 +81,11 @@ describe('Panier', () => {
           expect(order.body.orderLines.length).to.be.greaterThan(0);
         });
 
-        // retourne sur la page produit et vérifie que le stock a diminué
+        cy.intercept('GET', '**/products/5').as('getProductAgain');
         cy.visit('/#/products/5');
-        cy.get('[data-cy=detail-product-stock]').invoke('text').then((newStockText) => {
-          const newStock = parseInt(newStockText);
+        cy.wait('@getProductAgain');
+        cy.get('[data-cy=detail-product-stock]').should('not.be.empty').invoke('text').then((newStockText) => {
+          const newStock = parseInt(newStockText.replace(/\D/g, ''));
           expect(newStock).to.eq(initialStock - 1);
         });
       });
